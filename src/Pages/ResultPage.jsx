8784d5c77f2565417668cb679999d743623e8efe
Rikuto Iwashita <02rikuto18@gmail.com>
2025-06-16 08:18:17 +0900

    Resultコンポーネントの作成、propsとcssを追加

--- src/Pages/ResultPage.jsx
+++ src/Pages/ResultPage.jsx
@@ -1,16 +1,17 @@
 import { Link, useLocation } from "react-router-dom";
 import { ROUTES } from "../const";
+import Result from "../components/Result/Result";
+import { useEffect, useState } from "react";
 
 export default function ResultPage() {
   const location = useLocation();
-  const maxQuizLen = location.state.maxQuizLen
-  const correctNumLen = location.state.correctNumLen
+  const maxQuizLen = location.state.maxQuizLen;
+  const correctNumLen = location.state.correctNumLen;
 
   return (
     <>
       <h1>Result</h1>
-      <p>あなたの正解数は...</p>
-      <p>{`全${maxQuizLen}門中、${correctNumLen}問正解でした！`}</p>
+      <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
       <br />
       <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
     </>
